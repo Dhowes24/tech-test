18c5c41148c106fd851b58d263177dc9bba06103
Derek Howes <D.howes24@gmail.com>
2019-08-07 20:59:00 -0400

    Forgot to add everything in last commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
 /* Gulp should compile sass and make client side JS compatible - feel free to add your own tasks! */
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+
+
+gulp.task('sass', function () {
+    return gulp.src('scss/styles.scss')
+        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('scss/styles.scss', gulp.series('sass'));
+    // Other watchers
+});

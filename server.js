e92629ecada1ac5b9eda60c9d5f4cb3ae2a128df
Derek Howes <D.howes24@gmail.com>
2019-08-09 03:35:23 -0400

    Responsive Icons and better UI

--- server.js
+++ server.js
@@ -15,11 +15,13 @@ var zipcodes = require('zipcodes');
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.locals.city="";
-app.locals.state="";
-app.locals.weatherSummary= "";
-app.locals.temp = "";
-app.locals.time = "";
+app.locals.city = "n/a";
+app.locals.state = "n/a";
+app.locals.weatherSummary = "n/a";
+app.locals.temp = "n/a";
+app.locals.time = "n/a";
+
+app.locals.icon="n/a";
 
 // index page
 app.get('/', function (req, res) {
@@ -30,29 +32,50 @@ app.get('/', function (req, res) {
 app.post('/APIcall', async function (req, res) {
     var zipCode = req.body.ZipCode;
 
-    var location = zipcodes.lookup(zipCode);
-    // console.log(location);
+    try {
 
-    var lat = location.latitude;
-    var long = location.longitude;
+        var location = zipcodes.lookup(zipCode);
+        // console.log(location);
 
-    app.locals.city = location.city;
-    app.locals.state = location.state;
+        var lat = location.latitude;
+        var long = location.longitude;
 
-    await Request.get('https://api.darksky.net/forecast/'+apiKey+'/'+lat+','+long, (error, response, body) => {
-        if(error) {
-            return console.dir(error);
-        }
-        var json = JSON.parse(body);
+        app.locals.city = location.city;
+        app.locals.state = location.state;
 
-        app.locals.weatherSummary= json.currently.summary;
-        app.locals.temp = json.currently.temperature;
-        app.locals.time = json.currently.time;
+        await Request.get('https://api.darksky.net/forecast/' + apiKey + '/' + lat + ',' + long, (error, response, body) => {
+            if (error) {
 
-        console.dir(json.currently.summary);
+                app.locals.city = "n/a";
+                app.locals.state = "n/a";
+                app.locals.weatherSummary = "n/a";
+                app.locals.temp = "n/a";
+                app.locals.time = "n/a";
+                return console.dir(error);
+            } else {
+                var json = JSON.parse(body);
+
+                app.locals.weatherSummary = json.currently.summary;
+                app.locals.temp = json.currently.temperature;
+                app.locals.time = json.currently.time;
+                app.locals.icon = json.currently.icon;
+
+                console.dir(json.currently.summary);
+            }
+
+            res.redirect("/");
+        });
+    }
+    catch (e) {
+        app.locals.city = "n/a";
+        app.locals.state = "n/a";
+        app.locals.weatherSummary = "n/a";
+        app.locals.temp = "n/a";
+        app.locals.time = "n/a";
 
         res.redirect("/");
-    });
+
+    }
 
 });
 

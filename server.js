1c88d930f258a5b2c96e6a6e561184dfa864b563
Derek Howes <D.howes24@gmail.com>
2019-08-08 22:39:28 -0400

    Getting and displaying all data from zipcodes

--- server.js
+++ server.js
@@ -1,31 +1,59 @@
 // server.js
 // load the things we need
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+var apiKey = "c891518983807e7a46197e4de31eb2b9";
+var Request = require("request");
+var zipcodes = require('zipcodes');
+
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
+app.locals.city="";
+app.locals.state="";
+app.locals.weatherSummary= "";
+app.locals.temp = "";
+app.locals.time = "";
 
 // index page
-app.get('/', function(req, res) {
-    var drinks = [
-        { name: 'Bloody Mary', drunkness: 3 },
-        { name: 'Martini', drunkness: 5 },
-        { name: 'Scotch', drunkness: 10 }
-    ];
-    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
-
-    res.render('pages/index', {
-        drinks: drinks,
-        tagline: tagline
-    });
+app.get('/', function (req, res) {
+    res.render('pages/index');
 });
 
-// about page
-app.get('/about', function(req, res) {
-    res.render('pages/about');
+
+app.post('/APIcall', async function (req, res) {
+    var zipCode = req.body.ZipCode;
+
+    var location = zipcodes.lookup(zipCode);
+    // console.log(location);
+
+    var lat = location.latitude;
+    var long = location.longitude;
+
+    app.locals.city = location.city;
+    app.locals.state = location.state;
+
+    await Request.get('https://api.darksky.net/forecast/'+apiKey+'/'+lat+','+long, (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+        var json = JSON.parse(body);
+
+        app.locals.weatherSummary= json.currently.summary;
+        app.locals.temp = json.currently.temperature;
+        app.locals.time = json.currently.time;
+
+        console.dir(json.currently.summary);
+
+        res.redirect("/");
+    });
+
 });
 
 app.use(express.static(__dirname + '/'));
